0617b0a1afa079e89aa9ebef152aeba5574c608f
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2015-05-08 14:38:57 +0300

    tests

--- tests/SlugifyTest.php
+++ tests/SlugifyTest.php
@@ -13,8 +13,6 @@ namespace Cocur\Slugify\Tests;
 
 use Cocur\Slugify\Slugify;
 
-function locale_get_default() { return 'de_DE'; }
-
 /**
  * SlugifyTest
  *
@@ -162,6 +160,7 @@ class SlugifyTest extends \PHPUnit_Framework_TestCase
             array('H+e#l1l--o/W§o r.l:d)', 'h-e-l1l-o-w-o-r-l-d'),
             array(': World', 'world'),
             array('Hello World!', 'hello-world'),
+            array('Ä ä Ö ö Ü ü ß', 'a-a-o-o-u-u-s'),
             array('Á À á à É È é è Ó Ò ó ò Ñ ñ Ú Ù ú ù', 'a-a-a-a-e-e-e-e-o-o-o-o-n-n-u-u-u-u'),
             array('Â â Ê ê Ô ô Û û', 'a-a-e-e-o-o-u-u'),
             array('Â â Ê ê Ô ô Û 1', 'a-a-e-e-o-o-u-1'),
